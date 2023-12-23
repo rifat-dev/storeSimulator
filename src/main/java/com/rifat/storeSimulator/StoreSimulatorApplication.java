d200484959088f68dfdc6fe15bb86f702443fddb
Rifat <rifat0531m@gmail.com>
2023-12-23 21:14:54 +0300

    added /

--- src/main/java/com/rifat/storeSimulator/StoreSimulatorApplication.java
+++ src/main/java/com/rifat/storeSimulator/StoreSimulatorApplication.java
@@ -2,12 +2,20 @@ package com.rifat.storeSimulator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class StoreSimulatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreSimulatorApplication.class, args);
 	}
+
+	@GetMapping("/")
+    public String hello() {
+      return "hello";
+    }
 
 }
