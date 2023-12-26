a56a3947d9e20144f18bffdd7d25fbcbf8323ac5
Rifat <rifat0531m@gmail.com>
2023-12-26 18:08:12 +0300

    Added method for userRepository

--- src/main/java/com/rifat/storeSimulator/repository/UserRepository.java
+++ src/main/java/com/rifat/storeSimulator/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.rifat.storeSimulator.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.rifat.storeSimulator.model.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-
+    
+    Optional<User> findByLogin(String login);
 }
