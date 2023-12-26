ed325330e077e4c355cf24c0de61a22194e9d1c9
Rifat <rifat0531m@gmail.com>
2023-12-26 18:08:43 +0300

    Added userService

--- src/main/java/com/rifat/storeSimulator/service/UserService.java
+++ src/main/java/com/rifat/storeSimulator/service/UserService.java
@@ -1,5 +1,22 @@
 package com.rifat.storeSimulator.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.rifat.storeSimulator.model.User;
+import com.rifat.storeSimulator.repository.UserRepository;
+
 public class UserService {
     
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
