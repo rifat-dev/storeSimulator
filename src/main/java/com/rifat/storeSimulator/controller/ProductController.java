e0bf6df74925ef5d8ab1be0310b5be3f50783aa9
Rifat <rifat0531m@gmail.com>
2023-12-27 15:19:11 +0300

    Added new end-points

--- src/main/java/com/rifat/storeSimulator/controller/ProductController.java
+++ src/main/java/com/rifat/storeSimulator/controller/ProductController.java
@@ -1,9 +1,22 @@
 package com.rifat.storeSimulator.controller;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rifat.storeSimulator.model.Product;
 import com.rifat.storeSimulator.service.ProductService;
 
 @RestController
@@ -15,5 +28,32 @@ public class ProductController {
     @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable UUID id) {
+        Optional<Product> productOptional = productService.getProductById(id);
+        return productOptional.map(product -> ResponseEntity.ok().body(product))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<Product> getProductByName(@PathVariable String name) {
+        Optional<Product> productOptional = productService.getProductByName(name);
+        return productOptional.map(product -> ResponseEntity.ok().body(product))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok().body(products);
+    }
+
+    @PostMapping
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        Product savedProduct = productService.saveProduct(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/rifat/storeSimulator/controller/StoreController.java
+++ src/main/java/com/rifat/storeSimulator/controller/StoreController.java
@@ -1,9 +1,23 @@
 package com.rifat.storeSimulator.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rifat.storeSimulator.DTO.AvailableQuantityProductsInStoreDTO;
+import com.rifat.storeSimulator.DTO.CartForComparisonDTO;
+import com.rifat.storeSimulator.DTO.CartPurchaseDTO;
+import com.rifat.storeSimulator.model.ProductStore;
+import com.rifat.storeSimulator.model.Store;
 import com.rifat.storeSimulator.service.StoreService;
 
 @RestController
@@ -15,5 +29,65 @@ public class StoreController {
     @Autowired
     public StoreController(StoreService storeService) {
         this.storeService = storeService;
+    }
+
+    @PostMapping("/changeProductQuantity")
+    public ResponseEntity<Optional<ProductStore>> changeProductQuantity(
+            @RequestParam String storeName,
+            @RequestParam String productName,
+            @RequestParam int diffQuantity) {
+        Optional<ProductStore> result = storeService.changeProductQuantity(storeName, productName, diffQuantity);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/changeProductPrice")
+    public ResponseEntity<Optional<ProductStore>> changeProductPrice(
+            @RequestParam String storeName,
+            @RequestParam String productName,
+            @RequestParam int diffPrice) {
+        Optional<ProductStore> result = storeService.changeProductPrice(storeName, productName, diffPrice);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/findStoreWithCheapestProduct")
+    public ResponseEntity<Optional<ProductStore>> findStoreWithCheapestProduct(
+            @RequestParam String productName) {
+        Optional<ProductStore> result = storeService.findStoreWithCheapestProduct(productName);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/findQuantityProductsStoreMoney")
+    public ResponseEntity<List<AvailableQuantityProductsInStoreDTO>> findQuantityProductsStoreMoney(
+            @RequestParam String storeName,
+            @RequestParam double amount) {
+        List<AvailableQuantityProductsInStoreDTO> result = storeService.findQuantityProductsStoreMoney(storeName, amount);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/buyBatchOfProducts")
+    public ResponseEntity<Double> buyBatchOfProducts(
+            @RequestBody CartPurchaseDTO cartPurchaseDTO,
+            @RequestParam boolean changeQuantity) {
+        double result = storeService.buyBatchOfProducts(cartPurchaseDTO, changeQuantity);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/findCheapestBatchOfProducts")
+    public ResponseEntity<Store> findCheapestBatchOfProducts(
+            @RequestBody CartForComparisonDTO cartForComparisonDTO) {
+        Store result = storeService.findBatchOfProducts(cartForComparisonDTO);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/getAllStores")
+    public ResponseEntity<List<Store>> getAllStores() {
+        List<Store> result = storeService.getAllStores();
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/saveStore")
+    public ResponseEntity<Store> saveStore(@RequestBody Store store) {
+        Store savedStore = storeService.saveStore(store);
+        return new ResponseEntity<>(savedStore, HttpStatus.CREATED);
     }
 }
