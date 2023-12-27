3ea4a3ea73008e0d9e641440eafd8337dc4d7f08
Rifat <rifat0531m@gmail.com>
2023-12-27 15:18:42 +0300

    Disabled protection

--- src/main/java/com/rifat/storeSimulator/service/UserService.java
+++ src/main/java/com/rifat/storeSimulator/service/UserService.java
@@ -5,21 +5,23 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.authentication.BadCredentialsException;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.rifat.storeSimulator.DTO.NewUserDTO;
 import com.rifat.storeSimulator.DTO.ResponseRegisteredUserDTO;
-import com.rifat.storeSimulator.DTO.UserLoginDTO;
+// import com.rifat.storeSimulator.DTO.UserLoginDTO;
 import com.rifat.storeSimulator.model.User;
 import com.rifat.storeSimulator.repository.UserRepository;
 
+@Service
 public class UserService {
     
     private final UserRepository userRepository;
-    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    // private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Autowired
     public UserService(UserRepository userRepository) {
@@ -38,10 +40,12 @@ public class UserService {
         return userRepository.findByLogin(login);
     }
 
+    // доделать
     public ResponseRegisteredUserDTO createUser(NewUserDTO newUserDTO) {
         User user = new User();
         user.setLogin(newUserDTO.getLogin());
-        user.setHashPassword(hashPassword(newUserDTO.getPassword()));
+        // user.setHashPassword(hashPassword(newUserDTO.getPassword()));
+        user.setHashPassword(newUserDTO.getPassword());
         user.setRole(newUserDTO.getRole());
         userRepository.save(user);
 
@@ -51,25 +55,26 @@ public class UserService {
         return responseRegisteredUserDTO;
     }
 
-    public String loginUser(UserLoginDTO userLoginDTO) throws Exception {
-        Optional<User> userOptional = userRepository.findByLogin(userLoginDTO.getLogin());
+    // доделать
+    // public String loginUser(UserLoginDTO userLoginDTO) throws Exception {
+    //     Optional<User> userOptional = userRepository.findByLogin(userLoginDTO.getLogin());
 
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            if (passwordEncoder.matches(userLoginDTO.getPassword(), user.getHashPassword())) {
-                // доделать
-                return  "";
-            }
-        }
-        throw new BadCredentialsException("Error: Check login or password");
-    }
+    //     if (userOptional.isPresent()) {
+    //         User user = userOptional.get();
+    //         if (passwordEncoder.matches(userLoginDTO.getPassword(), user.getHashPassword())) {
+    //         
+    //             return  "";
+    //         }
+    //     }
+    //     throw new BadCredentialsException("Error: Check login or password");
+    // }
 
     public void deleteUser(UUID userId) {
         userRepository.deleteById(userId);
     }
 
-    private static String hashPassword(String password) {
-        return passwordEncoder.encode(password);
-    }
+    // private static String hashPassword(String password) {
+    //     return passwordEncoder.encode(password);
+    // }
 
 }
